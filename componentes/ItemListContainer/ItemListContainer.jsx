2612c9b2f65d5dad60e6647b17d17742c1f53340
Gabriela Ripoll <mariagabriela.ripoll@gmail.com>
2023-08-12 13:42:11 -0300

    dos

--- componentes/ItemListContainer/ItemListContainer.jsx
+++ componentes/ItemListContainer/ItemListContainer.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react'
-import ItemList from '../ItemList/ItemList'
+import ItemList from '../ItemList/itemList'
 import { useParams } from 'react-router-dom'
 import { collection, getDocs, query, where } from "firebase/firestore"
 import { db } from "../../src/firebase/config"
